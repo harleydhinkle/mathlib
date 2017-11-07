0c4deacbc2ef5e684a618d757d34b93add7e1d80
s179057 <s179057@SEA-8CC916>
2017-11-06 17:55:10 -0800

    yo

--- astoyed game/main.cpp
+++ astoyed game/main.cpp
@@ -1,5 +1,11 @@
+#include "transform.h"
 #include "sfwdraw.h"
-
+#include "mat3.h"
+#include "rigidbody.h"
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <cassert>
 int main()
 {
 	sfw::initContext();
